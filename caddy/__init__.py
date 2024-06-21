0964f2465427b37635841dac219f4ff8fadbc4f6
Christian Heider Nielsen <mrnaah@gmail.com>
2024-06-21 15:25:57 +0200

    pipeline

---
+++ caddy/__init__.py
@@ -0,0 +1,19 @@
+PROJECT_NAME = "Caddy"
+PROJECT_AUTHOR = "Christian Heider Lindbjerg <chen(at)mapspeople.com>"
+PROJECT_YEAR = 2024
+PROJECT_ORGANISATION = "MapsPeople"
+PROJECT_VERSION = "0.0.1"
+
+__doc__ = f"""
+Migration Base Module
+
+=====================   ======================
+project name:           {PROJECT_NAME}
+project author:         {PROJECT_AUTHOR}
+project organisation:   {PROJECT_ORGANISATION}
+project version:        {PROJECT_VERSION}
+project year:           {PROJECT_YEAR}
+=====================   ======================
+"""
+
+__version__ = PROJECT_VERSION
---
+++ docs/source/getting_started.rst
@@ -0,0 +1,6 @@
+.. _getting_started:
+
+Getting Started
+================
+Let's get started with a simple example of how to use upload geodata to an empty MapsIndoors Solution.
+Note that a solution needs to exists in MapsIndoors already.

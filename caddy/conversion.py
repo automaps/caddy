c42bd68632494f816ce867fc6afb8426adf201dd
Christian Heider Nielsen <mrnaah@gmail.com>
2024-06-25 15:17:50 +0200

    block inserts

--- caddy/conversion.py
+++ caddy/conversion.py
@@ -1,28 +1,67 @@
 import logging
-from typing import Iterable, Optional, Tuple
+from copy import copy, deepcopy
+from dataclasses import dataclass
+from typing import Iterable, Optional, Tuple, Union
 
 import shapely
 from ezdxf.addons import geo
-from ezdxf.entities import DXFEntity, DXFGraphic, Insert, MText, Text
-from ezdxf.math import Matrix44
-from ezdxf.query import EntityQuery
+from ezdxf.entities import DXFEntity, DXFGraphic, Dimension, Insert, MText, Text
+from ezdxf.layouts import BlockLayout
+from ezdxf.math import Matrix44, basic_transformation
 
 TRANSFORM = False
 logger = logging.getLogger(__name__)
 
-__all__ = ["to_shapely"]
+__all__ = ["to_shapely", "BlockInsertion"]
+
+
+@dataclass
+class BlockInsertion:
+    block: BlockLayout
+    insertion: Insert
 
 
 def to_shapely(
-    entity: DXFEntity, m: Matrix44 = None, step_size: float = 0.1
-) -> Optional[Tuple[shapely.geometry.base.BaseGeometry, DXFEntity]]:
+    entity: DXFEntity,
+    m: Matrix44 = None,
+    step_size: float = 0.1,
+    precision: float = 10,
+) -> Optional[Tuple[shapely.geometry.base.BaseGeometry, Union[DXFEntity, BlockLayout]]]:
     if isinstance(entity, Insert):
-        entities: EntityQuery = entity.explode()
-        # entities.groupby()
-        for ent in entities:
-            yield from to_shapely(ent, m)
+        if False:
+            multiplier = 10
+            vec3 = entity.dxf.insert
 
-        return
+            x, y, z = vec3.x, vec3.y, vec3.z
+            rotation = entity.dxf.rotation
+            xs, ys, zs = entity.dxf.xscale, entity.dxf.xscale, entity.dxf.xscale
+
+            left_ = vec3 * basic_transformation(
+                (x - multiplier, y - multiplier, z - multiplier), z_rotation=rotation
+            )
+
+            to_ = vec3 * basic_transformation(
+                (xs * multiplier, ys * multiplier, zs * multiplier), z_rotation=rotation
+            )
+
+            yield shapely.LineString(
+                [(left_.x, left_.y), (x, y), (to_.x, to_.y)]
+                # , vec3.z
+            ), entity.block()
+            # TODO: Box of scale WITH THE BEARING from rotation
+        else:
+            vec3 = entity.dxf.insert
+
+            x, y, z = vec3.x, vec3.y, vec3.z
+            yield shapely.Point(x, y), BlockInsertion(
+                entity.block(), entity
+            )  # deepcopy(entity))
+
+        entity_query = (
+            entity.virtual_entities()
+        )  # entity.explode() # WARNING, PERMUTEs THE insert entity
+        for ent in entity_query:
+            yield from to_shapely(ent, m)
 
     elif isinstance(entity, (MText, Text)):
         vec3 = entity.dxf.insert
@@ -32,11 +71,14 @@ def to_shapely(
             # , vec3.z
         ), entity
 
+    elif isinstance(entity, Dimension):
+        logger.info(f"Skipping dimension conversion {entity}")
+
     elif isinstance(entity, (DXFGraphic, Iterable)):
         try:
             geo_proxy = geo.proxy(entity, distance=step_size, force_line_string=False)
 
-            geo_proxy.places = 10  # Infinite precision
+            geo_proxy.places = precision
 
             if TRANSFORM:
                 # Transform DXF WCS coordinates into CRS coordinates:
@@ -54,3 +96,7 @@ def to_shapely(
     else:
         logger.warning(entity)
         ...
+
+    # logger.warning("Done")
+
+    return
--- caddy/to_gpkg.py
+++ caddy/to_gpkg.py
@@ -1,8 +1,8 @@
+import logging
 from collections import OrderedDict, defaultdict
 from pathlib import Path
 
 import ezdxf
-import shapely
 from ezdxf.math import Matrix44
 from geopandas import GeoDataFrame
 
@@ -10,12 +10,14 @@ __all__ = ["export_to_gpkg"]
 
 from jord.shapely_utilities.base import clean_shape
 from jord.shapely_utilities.polygons import ensure_cw_poly, is_polygonal
+from ezdxf.entities import DXFEntity, MText, Text, Insert
+from .conversion import to_shapely, BlockInsertion
 
-from ezdxf.entities import DXFEntity, DXFGraphic, Insert, MText, Text
-from .conversion import to_shapely
+
+logger = logging.getLogger(__name__)
 
 
-def export_to_gpkg(dxf_path: Path, out_path: Path, driver="GPKG") -> None:
+def export_to_gpkg(dxf_path: Path, out_path: Path, driver: str = "GPKG") -> None:
     source_doc = ezdxf.readfile(str(dxf_path))
 
     msp = source_doc.modelspace()
@@ -31,15 +33,45 @@ def export_to_gpkg(dxf_path: Path, out_path: Path, driver="GPKG") -> None:
         # Identity matrix for DXF files without geo reference data:
         m = Matrix44()
 
+    logger.warning(f"Converting {dxf_path}")
+
     geoms = defaultdict(list)
     for entity in msp.query("*"):
         for g, e in to_shapely(entity, m):
             if g:
-                cleaned = clean_shape(g)
-                if False:
-                    if is_polygonal(cleaned):
-                        cleaned = ensure_cw_poly(cleaned)
-                geoms[e.dxf.layer].append((cleaned, e))
+                if isinstance(e, DXFEntity):
+                    cleaned = clean_shape(g)
+                    if False:
+                        if is_polygonal(cleaned):
+                            cleaned = ensure_cw_poly(cleaned)
+                    geoms[e.dxf.layer].append((cleaned, e))
+                elif isinstance(e, BlockInsertion):
+                    logger.warning(f"Found block layout {e}")
+                    geoms[f"BLOCK_INSERTS_OF_{e.block.name}"].append((g, e.insertion))
+                else:
+                    logger.error(f"Unexpected entity type {type(e)}")
+            else:
+                logger.error(f"{entity} has no geometry ")
+
+    for block in source_doc.blocks:
+        for entity in block.entity_space:
+            for g, e in to_shapely(entity, m):
+                if g:
+                    if isinstance(e, DXFEntity):
+                        cleaned = clean_shape(g)
+                        if False:
+                            if is_polygonal(cleaned):
+                                cleaned = ensure_cw_poly(cleaned)
+                        geoms[f"BLOCK_{block.name}"].append((cleaned, e))
+                    elif isinstance(e, BlockInsertion):
+                        logger.warning(f"Found block layout {e}")
+                        geoms[f"BLOCK_{block.name}"].append((g, e.insertion))
+                    else:
+                        logger.error(f"Unexpected entity type {type(e)}")
+                else:
+                    logger.error(f"{entity} has no geometry ")
+
+    logger.warning(f"Exporting {dxf_path} -> {out_path}")
 
     for l, ges in dict(geoms).items():
         extras = defaultdict(list)
@@ -52,9 +84,16 @@ def export_to_gpkg(dxf_path: Path, out_path: Path, driver="GPKG") -> None:
                     extras["text"].append(e.plain_text())
                 else:
                     extras["text"].append(e.dxf.text)
+            elif isinstance(e, Insert):
+                rotation = e.dxf.rotation
+                xs, ys, zs = e.dxf.xscale, e.dxf.xscale, e.dxf.xscale
+
+                extras["text"].append(f"{rotation=} {xs=} {ys=} {zs=}")
             else:
                 extras["text"].append("")
 
         gdf = GeoDataFrame({"geometry": g, **extras})
         # gdf.crs = 'EPSG:4326'
         gdf.to_file(str(out_path), driver=driver, layer=l)
+
+    logger.warning(f"Wrote {out_path}")

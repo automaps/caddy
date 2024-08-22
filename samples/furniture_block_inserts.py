3859fa9801d9c391e2174be37afd8a6706d278e0
Christian Heider Nielsen <mrnaah@gmail.com>
2024-08-22 19:45:40 +0200

    oh format

--- samples/furniture_block_inserts.py
+++ samples/furniture_block_inserts.py
@@ -12,47 +12,50 @@ from caddy.exporting import get_block_geoms
 from caddy.helpers import get_transformation, plot_shapely_geometry, random_rgba
 
 if __name__ == "__main__":
-  def auh(plot: bool = False) -> None:
-    color_generator = iter(random_rgba())
 
-    masks = defaultdict(list)
+    def auh(plot: bool = False) -> None:
+        color_generator = iter(random_rgba())
 
-    files = list(
-        Path(r"S:\Geodata\Indoor\FordMotors\5091\geodata\cad\dxf").rglob("*fur.dxf")
+        masks = defaultdict(list)
+
+        files = list(
+            Path(r"S:\Geodata\Indoor\FordMotors\5091\geodata\cad\dxf").rglob("*fur.dxf")
         )
 
-    for f in progress_bar(files):
-      for block_name, block in progress_bar(get_block_geoms(f).items()):
-        geoms = shapely.GeometryCollection(block["geometries"])
+        for f in progress_bar(files):
+            for block_name, block in progress_bar(get_block_geoms(f).items()):
+                geoms = shapely.GeometryCollection(block["geometries"])
 
-        buffered = shapely.unary_union(
-            dilate(shapely.unary_union(geoms), distance=1)
+                buffered = shapely.unary_union(
+                    dilate(shapely.unary_union(geoms), distance=1)
+                )
+
+                for insertion_point in progress_bar(block["inserts"]):
+                    transformed_geoms = affine_transform(
+                        buffered, get_transformation(insertion_point)
+                    )
+
+                    if plot:
+                        plot_shapely_geometry(
+                            transformed_geoms, color=next(color_generator)
+                        )
+
+                    masks[block_name].append(transformed_geoms)
+
+            gdf = GeoDataFrame(
+                (
+                    {"name": block_name, "geometry": instance}
+                    for block_name, instances in masks.items()
+                    for instance in instances
+                ),
+                geometry="geometry",
+                crs=3857,
             )
 
-        for insertion_point in progress_bar(block["inserts"]):
-          transformed_geoms = affine_transform(
-              buffered, get_transformation(insertion_point)
-              )
+            with open(f"{f.stem}-blocks.geojson", "w") as f:
+                f.write(gdf.to_json())
 
-          if plot:
-            plot_shapely_geometry(transformed_geoms, color=next(color_generator))
+            if plot:
+                pyplot.show()
 
-          masks[block_name].append(transformed_geoms)
-
-      gdf = GeoDataFrame(
-          (
-              {"name": block_name, "geometry": instance}
-              for block_name, instances in masks.items()
-              for instance in instances
-              ),
-          geometry="geometry",
-          crs=3857,
-          )
-
-      with open(f"{f.stem}-blocks.geojson", "w") as f:
-        f.write(gdf.to_json())
-
-      if plot:
-        pyplot.show()
-
-  auh()
+    auh()
--- tests/test_import.py
+++ tests/test_import.py
@@ -4,11 +4,13 @@ from warg import ensure_in_sys_path
 
 ensure_in_sys_path(Path(__file__).parent.parent)
 
-def test_import_package():
-  if True:
-    import caddy
 
-    print(caddy.__version__)
+def test_import_package():
+    if True:
+        import caddy
+
+        print(caddy.__version__)
+
 
 def test_math():
-  assert 1 + 1 == 2
+    assert 1 + 1 == 2

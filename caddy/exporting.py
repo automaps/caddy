8acbc65c4d96e6b7b6e17b918c6321af0dd4cf9b
MAPSPEOPLE\nion <nion@mapspeople.com>
2024-09-10 11:12:04 +0200

    added copy of export to function, removed the outpaths and driver parts and it now returns the list of dictionaries.

--- caddy/exporting.py
+++ caddy/exporting.py
@@ -155,6 +155,96 @@ def export_to(
     logger.warning(f"Wrote {out_path}")
 
 
+def export_to_shapely_dict(
+    dxf_path: Path
+) -> List[Dict]:
+
+    source_doc = ezdxf.readfile(str(dxf_path))
+
+    msp = source_doc.modelspace()
+
+    # Store geo located DXF entities as GeoJSON data:
+    # Get the geo location information from the DXF file:
+    geo_data = msp.get_geodata()
+
+    if geo_data:
+        # Get transformation matrix and epsg code:
+        m, epsg = geo_data.get_crs_transformation()
+    else:
+        # Identity matrix for DXF files without geo reference data:
+        m = Matrix44()
+
+    logger.warning(f"Converting {dxf_path}")
+
+    geoms = defaultdict(list)
+    for entity in msp.query("*"):
+        for g, e in to_shapely(entity, m):
+            if g:
+                if isinstance(e, DXFEntity):
+                    cleaned = clean_shape(g)
+                    if False:
+                        if is_polygonal(cleaned):
+                            cleaned = ensure_cw_poly(cleaned)
+                    geoms[e.dxf.layer].append((cleaned, e))
+                elif isinstance(e, BlockInsertion):
+                    # logger.warning(f"Found block layout {e}")
+                    geoms[f"BLOCK_INSERTS_OF_{e.block.name}"].append((g, e.insertion))
+                elif isinstance(e, FailCase):
+                    # logger.warning(f"Found fail case {e}")
+                    geoms[f"FAIL_CASE_{e.entity.dxf.layer}"].append((g, e.entity))
+
+                else:
+                    logger.error(f"Unexpected entity type {type(e)}")
+            else:
+                logger.error(f"{entity} has no geometry ")
+
+    if False:
+        for block in source_doc.blocks:
+            for entity in block.entity_space:
+                for g, e in to_shapely(entity, m):
+                    if g:
+                        if isinstance(e, DXFEntity):
+                            cleaned = clean_shape(g)
+                            if False:
+                                if is_polygonal(cleaned):
+                                    cleaned = ensure_cw_poly(cleaned)
+                            geoms[f"BLOCK_{block.name}"].append((cleaned, e))
+                        elif isinstance(e, BlockInsertion):
+                            # logger.warning(f"Found block layout {e}")
+                            geoms[f"BLOCK_{block.name}"].append((g, e.insertion))
+
+                        else:
+                            logger.error(f"Unexpected entity type {type(e)}")
+                    else:
+                        logger.error(f"{entity} has no geometry ")
+
+
+
+    extras = defaultdict(list)
+    for l, ges in dict(geoms).items():
+        g, e = zip(*ges)
+
+        for e in e:
+            if isinstance(e, (MText, Text)):
+                if hasattr(e, "plain_text"):
+                    extras["text"].append(e.plain_text())
+                else:
+                    extras["text"].append(e.dxf.text)
+            elif isinstance(e, Insert):
+                rotation = e.dxf.rotation
+                xs, ys, zs = e.dxf.xscale, e.dxf.xscale, e.dxf.xscale
+
+                extras["text"].append(f"{rotation=} {xs=} {ys=} {zs=}")
+            else:
+                extras["text"].append("")
+
+        for g_ in g:
+            extras["geometry"].append(g_)
+            extras["layer"].append(l)
+
+    return extras
+
+
 @dataclass
 class BlockPointInsert:
     point: shapely.Point
